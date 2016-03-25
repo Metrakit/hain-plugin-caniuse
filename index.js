f95859e3f45eafbeea0e351c7846b460cca0c253
metrakit <contact@jordane.net>
2016-03-25 12:41:00 +0100

    fix bugs and refacto

--- index.js
+++ index.js
@@ -1,33 +1,84 @@
+/*jshint esversion: 6 */
+/*jshint globalstrict: true */
+
 'use strict';
 
 const _       = require('lodash');
-const caniuse = require('caniuse-api');
+const caniuse = require('./caniuse-api');
 
 module.exports = (pluginContext) => {
     const shell = pluginContext.shell;
 
+    const browsers = ['firefox', 'chrome', 'ie', 'opera', 'safari'];
+
+    function capitalize(word) {
+      return word[0].toUpperCase() + word.slice(1);
+    }
+
+    function getDesc(property) {
+        const support = caniuse.getSupport(property);
+        var desc = "";
+
+        _.take(browsers, 5).map((x) => {
+
+          if (x == 'ie') {
+            desc = desc + 'IE <b>';
+          } else {
+            desc = desc + capitalize(x) + ' <b>';
+          }
+          if (support[x].y === undefined) {
+            desc = desc + '✘';
+          } else {
+            desc = desc + support[x].y + '+';
+          }
+          desc = desc + '</b>, ';
+        });
+
+        return desc.slice(0, -2);
+    }
+
     function search(query, res) {
         const query_trim = query.trim();
-        if (query_trim.length == 0) {
-            return;
+        if (query_trim.length === 0) {
+            return res.add({
+            id: '',
+            payload: 'open',
+            title: 'Search on caniuse',
+            desc: 'Search on caniuse.com'
+          });
         }
 
-        let results = caniuse.find(query_trim);
-
-        _.take(results, 5).map((x) => {
-          const support = caniuse.getSupport(query_trim);
-          const desc = 'Chrome <b>' + support.chrome.y + '+</b>, '
-          + 'Firefox <b>' + support.firefox.y + '+</b>, '
-          + 'IE <b>' + support.ie.y + '+</b>, '
-          + 'Safari <b>' + support.safari.y + '+</b>, '
-          + 'Opera <b>' + support.opera.y + '+</b>';
-          return res.add({
-              id: x,
-              payload: 'open',
-              title: 'Search ' + x,
-              desc: desc
-          });
+        res.add({
+            id: query_trim,
+            payload: 'open',
+            title: 'Search property: ' + query_trim,
+            desc: 'Search on caniuse.com'
         });
+
+        let results = caniuse.find(query_trim);
+        if (typeof results === "string") {
+
+          return res.add({
+              id: results,
+              payload: 'open',
+              title: results,
+              desc: getDesc(results)
+          });
+        } else {
+          _.take(results, 5).map((x) => {
+
+            if (x == query_trim) {
+              res.remove(query_trim);
+            }
+
+            return res.add({
+                id: x,
+                payload: 'open',
+                title: x,
+                desc: getDesc(x)
+            });
+          });
+        }
     }
 
     function execute(id, payload) {
